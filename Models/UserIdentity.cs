9984640f2fbd0a4c4e38519850c0ce01957c9b9d
C'est La Vie <18225083149@163.com>
2023-06-21 16:22:29 +0800

    1

---
+++ Models/UserIdentity.cs
@@ -0,0 +1,28 @@
+﻿namespace FlowLabourApi.Models
+{
+    /// <summary>
+    /// 用户-身份信息表,对应数据库表: useridentity
+    /// </summary>
+    public partial class UserIdentity
+    {
+        /// <summary>
+        /// 对应auth_user表中的id。
+        /// </summary>
+        public int UserId { get; set; }
+
+        /// <summary>
+        /// 对应identityinfo表中的id。
+        /// </summary>
+        public int IdentityId { get; set; }
+
+        /// <summary>
+        /// 用户信息
+        /// </summary>
+        public virtual AuthUser User { get; set; } = null!;
+
+        /// <summary>
+        /// 真实身份信息
+        /// </summary>
+        public virtual IdentityInfo Identity { get; set; } = null!;
+    }
+}
