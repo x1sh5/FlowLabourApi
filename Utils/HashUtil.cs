e410e1d0f945d4e81982e9b6b4bae806249ffbb8
C'est La Vie <18225083149@163.com>
2023-06-27 17:51:48 +0800

    1

--- Utils/HashUtil.cs
+++ Utils/HashUtil.cs
@@ -7,14 +7,14 @@
             using (System.Security.Cryptography.SHA256 sha256Hash = System.Security.Cryptography.SHA256.Create())
             {
                 byte[] bytes = sha256Hash.ComputeHash(System.Text.Encoding.UTF8.GetBytes(input));
-                //System.Text.StringBuilder builder = new System.Text.StringBuilder();
-                //foreach (byte b in bytes)
-                //{
-                //    builder.Append(b.ToString("x2"));
-                //}
-                string hash = BitConverter.ToString(bytes).Replace("-", string.Empty);
-                //return builder.ToString();
-                return hash;
+                System.Text.StringBuilder builder = new System.Text.StringBuilder();
+                foreach (byte b in bytes)
+                {
+                    builder.Append(b.ToString("x2"));
+                }
+                //string hash = BitConverter.ToString(bytes).Replace("-", string.Empty);
+                return builder.ToString();
+                //return hash;
             }
         }
     }
