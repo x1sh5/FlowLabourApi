a83811345ad5c52a33706c1547b2d0efddc5b8c0
C'est La Vie <18225083149@163.com>
2023-07-31 03:14:13 +0800

    1

--- Hubs/ChatHub.cs
+++ Hubs/ChatHub.cs
@@ -24,9 +24,43 @@ public class ChatHub : Hub
         _context = context;
     }
     
-    public async Task SendMessage(string user, string message)
+    public async Task SendMessage(IList<object> datas)
     {
-        Clients.All.SendAsync("ReceiveMessage", user, message);
+        int args = datas.Count;
+        switch (args) {
+            case 1:
+                await Clients.All.SendAsync("ReceiveMessage", datas[0]);
+                break;
+            case 2:
+                await Clients.All.SendAsync("ReceiveMessage", datas[0], datas[1]);
+                break;
+            case 3:
+                await Clients.All.SendAsync("ReceiveMessage", datas[0], datas[1], datas[2]);
+                break;
+            case 4:
+                await Clients.All.SendAsync("ReceiveMessage", datas[0], datas[1], datas[2], datas[3]);
+                break;
+            case 5:
+                await Clients.All.SendAsync("ReceiveMessage", datas[0], datas[1], datas[2], datas[3], datas[4]);
+                break;
+            case 6:
+                await Clients.All.SendAsync("ReceiveMessage", datas[0], datas[1], datas[2], datas[3], datas[4], datas[5]);
+                break;
+            case 7:
+                await Clients.All.SendAsync("ReceiveMessage", datas[0], datas[1], datas[2], datas[3], datas[4], datas[5], datas[6]);
+                break;
+            case 8:
+                await Clients.All.SendAsync("ReceiveMessage", datas[0], datas[1], datas[2], datas[3], datas[4], datas[5], datas[6], datas[7]);
+                break;
+            case 9:
+                await Clients.All.SendAsync("ReceiveMessage", datas[0], datas[1], datas[2], datas[3], datas[4], datas[5], datas[6], datas[7], datas[8]);
+                break;
+            case 10:
+                await Clients.All.SendAsync("ReceiveMessage", datas[0], datas[1], datas[2], datas[3], datas[4], datas[5], datas[6], datas[7], datas[8], datas[9]);
+                break;
+        }
+
+        //await Clients.All.SendAsync("ReceiveMessage", user, message);
     }
 
     public async Task SendToUser(string user, Message message)
